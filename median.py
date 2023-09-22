f8bd0ae97e41ac690eefe14290cce504029489be
Ibrahim Uddin <k21043852@kcl.ac.uk>
2023-09-22 23:26:34 +0100

    task1

--- median.py
+++ median.py
@@ -9,4 +9,12 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+length_of_list = len(numbers)
+if length_of_list%2!=0:
+    middle = int((length_of_list+1)/2)
+    print(middle)
+    print(numbers[middle-1])
+else:
+    middle = int(length_of_list /2)
+    average_of_both_middle = (numbers[middle-1] + numbers[middle])/2
+    print(average_of_both_middle)
